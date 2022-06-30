c33ef8697a50fbb524c8d54bf760197bb6d81dcb
shaurya127 <shaurya1272000@gmail.com>
2022-06-30 21:41:48 +0530

    initial commit

---
+++ src/components/Message.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Message(props) {
+  return (
+    
+      <div className="outerdiv">
+       
+
+        <div className="innerdiv">
+          <p className="desc">Send me anonymous messages!</p>
+        </div>
+       
+        <input type="text" placeholder="Type somthing..." className="inputbox"   />
+
+       
+        </div>
+        
+      
+    
+  );
+}
+
+export default Message;
